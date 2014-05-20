0e9d641a029f1856ffe17cc5036652f7dfa2e81b
Stefan Penner <stefan.penner@gmail.com>
2014-05-19 23:52:24 -0400

    WIP

--- tests/unit/tasks/server/livereload-server-test.js
+++ tests/unit/tasks/server/livereload-server-test.js
@@ -1,11 +1,22 @@
 'use strict';
 
 var assert           = require('../../../helpers/assert');
-var liveReloadServer = require('../../../../lib/tasks/server/livereload-server');
+var LiveReloadServer = require('../../../../lib/tasks/server/livereload-server');
+var MockUI           = require('../../../helpers/mock-ui');
 
 describe('livereload-server', function() {
-  it('should return immediately if `liveReload` option is false', function() {
-    return liveReloadServer.start({liveReload: false})
+  var subject;
+
+  beforeEach(function() {
+    subject = new LiveReloadServer({
+      ui: new MockUI()
+    });
+  });
+
+  it('returns immediately if `liveReload` option is false', function() {
+    return subject.start({
+        liveReload: false
+      })
       .then(function(result) {
         assert.equal('live-reload is disabled', result);
       });
